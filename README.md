180ee05d937bfad600ebb483ea559d204a668a6b
nchristanto <christanto.nanda@gmail.com>
2019-04-10 05:49:46 +0700

    "add function to index.js, edit readme"

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Project Javascript functions 
 
-Experimenting Javascript functions
+Experimenting Javascript functions:
+- Function to process sentence
+- Healthy Haku daily water intake calculator
+- Food nutrition facts
--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 // Example 1 
-// function logName(name) {
-//     console.log(`Hello, ${name}!`)
-// }
+function processWord(word) {
+    console.log(`Hello, ${word}!`)
+}
 
-// const nameA = 'Alpha'
-// const nameB = 'Betty'
+const wordA = 'Haku'
+const wordB = 'Healthy'
 
-// logName(nameA)
-// logName(nameB)
+processWord(`${wordB} ${wordA}`)
+
 
 // Example 2
-
-let recommendedWaterIntakePerKG = 0.184;
+// Health Haku Daily Water Intake Calculator
+let recommendedWaterIntakePerKG = 0.0435;
 let userWeight = prompt('How much you think your body weight?');
 
 const waterIntakeCalculator = function(weight) {
@@ -19,39 +19,36 @@ const waterIntakeCalculator = function(weight) {
     return userWaterGoal
 }
 
-// Test Cases, units in cups
-document.getElementById("water-goal").innerHTML = `You need to drink ${waterIntakeCalculator(userWeight)} cups of water per day` 
+// Test Cases, units in liters
+document.getElementById("water-goal").innerHTML = `You need to drink ${waterIntakeCalculator(userWeight)} liters of water per day` 
 console.log(waterIntakeCalculator(userWeight))
-console.log(waterIntakeCalculator(65))
+console.log(waterIntakeCalculator())
 console.log(waterIntakeCalculator(70))
 
 
 // Example 3 
-// const addMarks = (text, mark, times) => {
-//     let newText = text
+const addMarks = (text, mark, times) => {
+    let newText = text
 
-//     for (let i = 0; i <= times; i++) {
-//         newText += mark
-//     }
-//     console.log(newText)
-// }
+    for (let i = 0; i <= times; i++) {
+        newText += mark
+    }
+    console.log(newText)
+}
 
-// addMarks('Hello', '!', 1)
-// addMarks('Hello', '!', 3)
-// addMarks('How are you', '?', 1)
-// addMarks('How are you', '?', 2)
+addMarks('Hello, I am Healthy Haku', '!' , 1)
+addMarks('I will drink more water', '!', 2)
 
-// // Example 4
-// const showNameWithAge = (name = 'Unknown', age = 0) => {
-//     const nameWithAge = `${name} is ${age} year(s) old`
 
-//     return nameWithAge
-// }
+// Example 4
+const showFoodNutrition = (food = 'unknown', nutrition) => {
+    const foodNutrition = `${food} contains ${nutrition}`
 
-// const alpha = showNameWithAge
-// const betty = showNameWithAge
-// const gamma = showNameWithAge
+    return foodNutrition
+}
 
-// console.log(alpha)
-// console.log(betty)
-// console.log(gamma)
+const rice = showFoodNutrition('rice', 'carbohydrates')
+const egg = showFoodNutrition('egg', 'proteins')
+
+console.log(rice)
+console.log(egg)
